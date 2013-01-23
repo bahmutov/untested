f295e1fbc2429620fe1ca42bcb7ff8015380b46d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-01-22 22:42:57 -0500

    connected lasso to untested

--- src/runTests.js
+++ src/runTests.js
@@ -1,8 +1,10 @@
 var spawn = require('child_process').spawn;
 var path = require('path');
 var gt = require('gt/covered');
+var lasso = require('lasso-node');
 
-function _test (tool, test) {
+function jsTest(test) {
+	console.assert(test, 'undefined js test');
 	gt.init({
 		module: [test],
 		colors: true
@@ -10,10 +12,20 @@ function _test (tool, test) {
 	gt.run();
 }
 
+function pageTest(test) {
+	console.assert(test, 'undefined page test');
+	lasso.run({
+		page: test,
+		untested: true
+	});
+}
+
 function runTest (test) {
 	console.assert(test, 'expected a test');
 	if (/\.js$/.test(test)) {
-		_test('gt', test);
+		jsTest(test);
+	} else if (/\.html$/.test(test)) {
+		pageTest(test);
 	} else {
 		console.error('cannot determine how to run unit test', test);
 	}
